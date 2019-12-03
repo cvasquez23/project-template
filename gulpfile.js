12b1a7f342cd6d6a6c4d918bdcb6c03240dc8711
cvasquez23 <vasqu1cr@cmich.edu>
2019-12-03 18:02:27 -0500

    12.03.19

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+
+// compile scss into css
+function style() {
+  // where is the scss file
+  return gulp.src('./scss/**/*.scss')
+  // pass through scss compiler and minify
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  //rename
+  .pipe(rename({ suffix: '.min'}))
+  // where to save
+  .pipe(gulp.dest('./'))
+  // stream changes to all browsers
+  .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./js/.**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
